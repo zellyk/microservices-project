ae2849ba35d2a7f86e23a84ead4eb20f643ca88e
jonathan ma <jonathan.marcotte.7@gmail.com>
2021-04-14 17:16:29 -0400

    Recommendation Tests have been implemented

--- microservices/recommendation-service/src/test/java/com/marcotte/microservices/core/recommendation/RecommendationServiceApplicationTests.java
+++ microservices/recommendation-service/src/test/java/com/marcotte/microservices/core/recommendation/RecommendationServiceApplicationTests.java
@@ -1,10 +1,112 @@
 package com.marcotte.microservices.core.recommendation;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@AutoConfigureWebTestClient
 class RecommendationServiceApplicationTests {
+
+
+	private static final int PRODUCT_ID_OKAY = 1;
+	private static final int PRODUCT_ID_NOT_FOUND = 113;
+	private static final String PRODUCT_ID_INVALID_STRING = "not-integer";
+	private static final int PRODUCT_ID_INVALID_Negative_VALUE  = -1;
+
+	@Autowired
+	private WebTestClient client;
+
+
+	@Test
+	public void getRecommendationsByProductId(){
+
+		int expectedLength = 3;
+
+		client.get()
+				.uri("/recommendation?productId=" + PRODUCT_ID_OKAY)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.length()").isEqualTo(expectedLength)
+				.jsonPath("$[0].productId").isEqualTo(PRODUCT_ID_OKAY)
+				.jsonPath("$[1].productId").isEqualTo(PRODUCT_ID_OKAY)
+				.jsonPath("$[2].productId").isEqualTo(PRODUCT_ID_OKAY);
+
+
+	}
+
+	@Test
+	public void getRecommendationsMissingParameter(){
+
+		client.get()
+				.uri("/recommendation")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isBadRequest()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/recommendation")
+				.jsonPath("$.message").isEqualTo("Required int parameter 'productId' is not present");
+
+	}
+
+	@Test
+	public void getRecommendationsInvalidParameterString(){
+		client.get()
+				.uri("/recommendation?productId=" + PRODUCT_ID_INVALID_STRING)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isBadRequest()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/recommendation")
+				.jsonPath("$.message").isEqualTo("Type mismatch.");
+
+	}
+
+	@Test
+	public void getRecommendationsInvalidParameterNegativeValue(){
+		client.get()
+				.uri("/recommendation?productId=" + PRODUCT_ID_INVALID_Negative_VALUE)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/recommendation")
+				.jsonPath("$.message").isEqualTo("invalid productId:" + PRODUCT_ID_INVALID_Negative_VALUE);
+
+	}
+
+	@Test
+	public void getRecommendationsNotFound(){
+
+		int expectedLength = 0;
+
+		client.get()
+				.uri("/recommendation?productId=" + PRODUCT_ID_NOT_FOUND)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.length()").isEqualTo(expectedLength);
+
+	}
+
+
 
 	@Test
 	void contextLoads() {
