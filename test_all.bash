d34cb7e13859a5523023e8c75515798126762993
Jon Ma <jonathan.marcotte.7@gmail.com>
2021-04-16 11:29:48 -0400

    docker files initialized, TEST_BASH done

---
+++ test_all.bash
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+#
+# Sample usage:
+#
+#   HOST=localhost PORT=7000 ./test-em-all.bash
+#
+: ${HOST=localhost}
+: ${PORT=7000}
+
+function assertCurl() {
+
+  local expectedHttpCode=$1
+  local curlCmd="$2 -w \"%{http_code}\""
+  local result=$(eval $curlCmd)
+  local httpCode="${result:(-3)}"
+  RESPONSE='' && (( ${#result} > 3 )) && RESPONSE="${result%???}"
+
+  if [ "$httpCode" = "$expectedHttpCode" ]
+  then
+    if [ "$httpCode" = "200" ]
+    then
+      echo "Test OK (HTTP Code: $httpCode)"
+    else
+      echo "Test OK (HTTP Code: $httpCode, $RESPONSE)"
+    fi
+  else
+      echo  "Test FAILED, EXPECTED HTTP Code: $expectedHttpCode, GOT: $httpCode, WILL ABORT!"
+      echo  "- Failing command: $curlCmd"
+      echo  "- Response Body: $RESPONSE"
+      exit 1
+  fi
+}
+
+function assertEqual() {
+
+  local expected=$1
+  local actual=$2
+
+  if [ "$actual" = "$expected" ]
+  then
+    echo "Test OK (actual value: $actual)"
+  else
+    echo "Test FAILED, EXPECTED VALUE: $expected, ACTUAL VALUE: $actual, WILL ABORT"
+    exit 1
+  fi
+}
+set -e
+
+echo "HOST=${HOST}"
+echo "PORT=${PORT}"
+
+
+# Verify that a normal request works, expect three recommendations and three reviews
+
+
+# Verify that a 404 (Not Found) error is returned for a non existing productId (13)
+
+
+# Verify that no recommendations are returned for productId 113
+
+
+# Verify that no reviews are returned for productId 213
+
+
+# Verify that a 422 (Unprocessable Entity) error is returned for a productId that is out of range (-1)
+
+
+# Verify that a 400 (Bad Request) error error is returned for a productId that is not a number, i.e. invalid format
